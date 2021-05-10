24e04e305dc89cca5ff192eb731eecebf1a95161
Jiha <qpal748596@naver.com>
2021-05-10 16:22:50 +0900

    2021-05-10-v1

--- Java_Practice/src/java_20210509/MethodDemo.java
+++ Java_Practice/src/java_20210509/MethodDemo.java
@@ -2,30 +2,31 @@ package java_20210509;
 
 public class MethodDemo {
 	public int plus(int a, int b) {
-		return a+b;
+		return a + b;
 	}
+
 	public static void main(String[] args) {
 		MethodDemo m = new MethodDemo();
 		int sum = m.plus(10, 20);
 		System.out.println(sum);
-		System.out.printf("%d\n",sum);
-		
+		System.out.printf("%d\n", sum);
+
 		Calendar calendar = new Calendar();
 		boolean leafYear = calendar.isLeafYear(2100);
-		System.out.printf("%s\n",leafYear);
-		
-		int[] test = {10, 100, 30, 45, 70, 32, 55, 12, 99};
+		System.out.printf("%s\n", leafYear);
+
+		int[] test = { 10, 100, 30, 45, 70, 32, 55, 12, 99 };
 		Ascending asc = new Ascending();
 		Descending desc = new Descending();
 		int[] testAscending = asc.ascending(test);
 		for (int i = 0; i < testAscending.length; i++) {
-			System.out.printf("%d \t",testAscending );
+			System.out.printf("%d \t", testAscending[i]);
 		}
-		
+
 		int[] testDescending = desc.descending(test);
 		System.out.println();
 		for (int i = 0; i < testDescending.length; i++) {
-			System.out.printf("%d \t", testDescending);
+			System.out.printf("%d \t", testDescending[i]);
 		}
 		System.out.println();
 		System.out.println();
